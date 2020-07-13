bbf4384eca8fe467ff918da62a54377e41ccb231
Malachi Holden <malachi@suran.com>
2020-07-12 21:07:45 -0400

    add hardness and resistance to block properties

--- src/main/java/com/malachiholden/magnificentmagicalmod/ModBlocks.java
+++ src/main/java/com/malachiholden/magnificentmagicalmod/ModBlocks.java
@@ -8,6 +8,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModBlocks {
     public static final DeferredRegister <Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, MagnificentMagicalMod.MOD_ID);
-    public static final RegistryObject<Block> PEWTER_BLOCK=BLOCKS.register("pewter_block",()-> new Block(Block.Properties.create(Material.IRON)));
-    public static final RegistryObject<Block> SILVER_BLOCK=BLOCKS.register("silver_block",()-> new Block(Block.Properties.create(Material.IRON)));
+    public static final RegistryObject<Block> PEWTER_BLOCK=BLOCKS.register("pewter_block",()-> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3.5f, 6f)));
+    public static final RegistryObject<Block> SILVER_BLOCK=BLOCKS.register("silver_block",()-> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3.5f, 6f)));
 }
