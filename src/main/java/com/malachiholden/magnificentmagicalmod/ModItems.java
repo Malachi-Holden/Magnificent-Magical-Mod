14b89cecfee280238ed6f477dab031a9c895f433
exhanaver <64601950+exhanaver@users.noreply.github.com>
2020-07-13 12:28:50 -0400

    created copper block

--- src/main/java/com/malachiholden/magnificentmagicalmod/ModItems.java
+++ src/main/java/com/malachiholden/magnificentmagicalmod/ModItems.java
@@ -10,4 +10,5 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MagnificentMagicalMod.MOD_ID);
     public static final RegistryObject<Item> SILVER_BLOCK = ITEMS.register("silver_block", ()->new BlockItem(ModBlocks.SILVER_BLOCK.get(), new Item.Properties()));
     public static final RegistryObject<Item> PEWTER_BLOCK = ITEMS.register("pewter_block", ()->new BlockItem(ModBlocks.PEWTER_BLOCK.get(), new Item.Properties()));
+    public static final RegistryObject<Item> COPPER_BLOCK = ITEMS.register("copper_block", ()->new BlockItem(ModBlocks.COPPER_BLOCK.get(), new Item.Properties()));
 }
