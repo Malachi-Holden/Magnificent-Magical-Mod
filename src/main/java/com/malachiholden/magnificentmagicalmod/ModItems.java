a58f9970053764623157fcbdf1ff622b44c98f24
Malachi Holden <malachi@suran.com>
2020-07-13 21:45:25 -0400

    Fix tool type, added to block instead of item

--- src/main/java/com/malachiholden/magnificentmagicalmod/ModItems.java
+++ src/main/java/com/malachiholden/magnificentmagicalmod/ModItems.java
@@ -20,8 +20,8 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MagnificentMagicalMod.MOD_ID);
 
 //block item registry
-    public static final RegistryObject<Item> SILVER_BLOCK = ITEMS.register("silver_block", ()->new BlockItem(ModBlocks.SILVER_BLOCK.get(), (new Item.Properties()).maxStackSize(64).addToolType(ToolType.PICKAXE, 1).group(tabMagnificentMagicalMod)));
-    public static final RegistryObject<Item> PEWTER_BLOCK = ITEMS.register("pewter_block", ()->new BlockItem(ModBlocks.PEWTER_BLOCK.get(), (new Item.Properties()).maxStackSize(64).addToolType(ToolType.PICKAXE, 1).group(tabMagnificentMagicalMod)));
-    public static final RegistryObject<Item> COPPER_BLOCK = ITEMS.register("copper_block", ()->new BlockItem(ModBlocks.COPPER_BLOCK.get(), (new Item.Properties()).maxStackSize(64).addToolType(ToolType.PICKAXE, 1).group(tabMagnificentMagicalMod)));
+    public static final RegistryObject<Item> SILVER_BLOCK = ITEMS.register("silver_block", ()->new BlockItem(ModBlocks.SILVER_BLOCK.get(), (new Item.Properties()).maxStackSize(64).group(tabMagnificentMagicalMod)));
+    public static final RegistryObject<Item> PEWTER_BLOCK = ITEMS.register("pewter_block", ()->new BlockItem(ModBlocks.PEWTER_BLOCK.get(), (new Item.Properties()).maxStackSize(64).group(tabMagnificentMagicalMod)));
+    public static final RegistryObject<Item> COPPER_BLOCK = ITEMS.register("copper_block", ()->new BlockItem(ModBlocks.COPPER_BLOCK.get(), (new Item.Properties()).maxStackSize(64).group(tabMagnificentMagicalMod)));
 
 }
