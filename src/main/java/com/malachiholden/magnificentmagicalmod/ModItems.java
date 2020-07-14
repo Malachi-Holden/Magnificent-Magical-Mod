b833db74d4e496a869a8f2f60fb36e89a8a7224a
exhanaver <64601950+exhanaver@users.noreply.github.com>
2020-07-13 20:29:40 -0400

    added some properties to the existing blocks, added creative tab

--- src/main/java/com/malachiholden/magnificentmagicalmod/ModItems.java
+++ src/main/java/com/malachiholden/magnificentmagicalmod/ModItems.java
@@ -1,14 +1,27 @@
 package com.malachiholden.magnificentmagicalmod;
 
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+
 public class ModItems {
+
+    static final ItemGroup tabMagnificentMagicalMod = new ItemGroup("tabMagnificentMagicalMod") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(SILVER_BLOCK.get());
+        }
+    };
+
+
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MagnificentMagicalMod.MOD_ID);
-    public static final RegistryObject<Item> SILVER_BLOCK = ITEMS.register("silver_block", ()->new BlockItem(ModBlocks.SILVER_BLOCK.get(), new Item.Properties()));
-    public static final RegistryObject<Item> PEWTER_BLOCK = ITEMS.register("pewter_block", ()->new BlockItem(ModBlocks.PEWTER_BLOCK.get(), new Item.Properties()));
-    public static final RegistryObject<Item> COPPER_BLOCK = ITEMS.register("copper_block", ()->new BlockItem(ModBlocks.COPPER_BLOCK.get(), new Item.Properties()));
+
+//block item registry
+    public static final RegistryObject<Item> SILVER_BLOCK = ITEMS.register("silver_block", ()->new BlockItem(ModBlocks.SILVER_BLOCK.get(), (new Item.Properties()).maxStackSize(64).addToolType(ToolType.PICKAXE, 1).group(tabMagnificentMagicalMod)));
+    public static final RegistryObject<Item> PEWTER_BLOCK = ITEMS.register("pewter_block", ()->new BlockItem(ModBlocks.PEWTER_BLOCK.get(), (new Item.Properties()).maxStackSize(64).addToolType(ToolType.PICKAXE, 1).group(tabMagnificentMagicalMod)));
+    public static final RegistryObject<Item> COPPER_BLOCK = ITEMS.register("copper_block", ()->new BlockItem(ModBlocks.COPPER_BLOCK.get(), (new Item.Properties()).maxStackSize(64).addToolType(ToolType.PICKAXE, 1).group(tabMagnificentMagicalMod)));
+
 }
