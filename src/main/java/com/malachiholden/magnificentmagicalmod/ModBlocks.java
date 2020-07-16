7ea7a0c126a7721eb8db59f5647cf82ac2666584
exhanaver <64601950+exhanaver@users.noreply.github.com>
2020-07-16 19:07:36 -0400

    created base of copper cauldron

--- src/main/java/com/malachiholden/magnificentmagicalmod/ModBlocks.java
+++ src/main/java/com/malachiholden/magnificentmagicalmod/ModBlocks.java
@@ -16,4 +16,5 @@ public class ModBlocks {
     public static final RegistryObject<Block> BRASS_BLOCK=BLOCKS.register("brass_block",()-> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3.5f, 6f).harvestLevel(1).harvestTool(ToolType.PICKAXE)));
     public static final RegistryObject<Block> COPPER_ORE=BLOCKS.register("copper_ore",()-> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.5f,6f).harvestLevel(1).harvestTool(ToolType.PICKAXE)));
     public static final RegistryObject<Block> SILVER_ORE=BLOCKS.register("silver_ore",()-> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.5f,6f).harvestLevel(2).harvestTool(ToolType.PICKAXE)));
+    public static final RegistryObject<Block> COPPER_CAULDRON=BLOCKS.register("copper_cauldron",()-> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3.5f,6f).harvestLevel(1).harvestTool(ToolType.PICKAXE)));
 }
---
+++ src/main/java/com/malachiholden/magnificentmagicalmod/TranslucentBlock.java
@@ -0,0 +1,12 @@
+package com.malachiholden.magnificentmagicalmod;
+
+
+import net.minecraft.block.Block;
+
+public class TranslucentBlock extends Block {
+
+    public TranslucentBlock(Properties properties) {
+        super(properties);
+    }
+
+}
