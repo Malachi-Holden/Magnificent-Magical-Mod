71f01c76163f338f9d65eb066c41bc58e65ab22f
exhanaver <64601950+exhanaver@users.noreply.github.com>
2020-07-16 23:00:49 -0400

    finished the model for the copper cauldron

--- src/main/java/com/malachiholden/magnificentmagicalmod/WizardingCauldron.java
+++ src/main/java/com/malachiholden/magnificentmagicalmod/WizardingCauldron.java
@@ -5,11 +5,12 @@ import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
 public class WizardingCauldron extends Block {
 
-    public static final VoxelShape CAULDRON_SHAPE = VoxelShapeModel.fromModelJson("C:\\Users\\The Holdens\\IdeaProjects\\Magnificent-Magical-Mod\\src\\main\\resources\\assets\\magnificentmagicalmod\\modelscopper_cauldron.json");
+    public static final VoxelShape CAULDRON_SHAPE = VoxelShapes.create(3,0,13,13,10,13);
     public WizardingCauldron(Properties properties) {
         super(properties);
     }
