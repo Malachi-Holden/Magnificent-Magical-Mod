3f2c9add1279fcd4bd6a1fbbe9e38962c73dcf47
Malachi Holden <malachi@suran.com>
2020-07-17 19:04:28 -0400

    Use values from 0 to 1 instead of 0 to 16

--- src/main/java/com/malachiholden/magnificentmagicalmod/WizardingCauldron.java
+++ src/main/java/com/malachiholden/magnificentmagicalmod/WizardingCauldron.java
@@ -2,6 +2,7 @@ package com.malachiholden.magnificentmagicalmod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.CauldronBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
@@ -10,13 +11,14 @@ import net.minecraft.world.IBlockReader;
 
 public class WizardingCauldron extends Block {
 
-    public static final VoxelShape CAULDRON_SHAPE = VoxelShapes.create(3,0,13,13,10,13);
+    public static final VoxelShape CAULDRON_SHAPE = VoxelShapeModel.fromModelJson("assets/magnificentmagicalmod/models/block/copper_cauldron.json");
     public WizardingCauldron(Properties properties) {
         super(properties);
     }
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+//        VoxelShape superShape = super.getShape(state, worldIn, pos, context);
         return CAULDRON_SHAPE;
     }
 }
